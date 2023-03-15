2bb2a7c21eebf3f5d780ea8fb39454a65205afd4
Michael Maust <info@michaelmaust.com>
2023-03-15 14:56:20 -0500

    Starts adding seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Civilization;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        $civilizationData = require 'constants.php';
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        foreach ($civilizationData as $civilization) {
+        Civilization::create($civilization);
+        }
     }
 }
